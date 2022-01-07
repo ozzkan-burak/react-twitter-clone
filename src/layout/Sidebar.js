eb327a8156be6aa93f2878bab58c99d48763c2f8
Burak Özkan <burak.ozkan@path.com.tr>
2022-01-07 23:44:55 +0300

    episode1

--- src/layout/Sidebar.js
+++ src/layout/Sidebar.js
@@ -1,41 +1,87 @@
-import React from 'react'
-// icons
-import {HomeIcon} from "../icons/icon";
-import {MoreIcon} from "../icons/icon";
-import {ProfileIcon} from "../icons/icon";
-import {LitsIcon} from "../icons/icon";
-import {BookmarkIcon} from "../icons/icon";
-import {MessagesIcon} from "../icons/icon";
-import {NotificationIcon} from "../icons/icon";
-import {ExploreIcon} from "../icons/icon";
-// header icons
-import {MediaIcon} from "../icons/icon";
-import {GifIcon} from "../icons/icon";
-import {SurveyIcon} from "../icons/icon";
-import {EmojiyIcon} from "../icons/icon";
-import {ScheduleIcon} from "../icons/icon";
+import React, { useState } from "react";
+import twitterLogo from "../images/twitter.svg";
+//custom components
+import NavigationButton from "../components/NavigationButton";
+import {
+  MoreIcon,
+  ProfileIcon,
+  LitsIcon,
+  BookmarkIcon,
+  MessagesIcon,
+  NotificationIcon,
+  ExploreIcon,
+  HomeIcon,
+} from "../icons/icon";
+import UserBox from "../components/UserBox";
+
+const navButtons = [
+  {
+    name: "Home",
+    icon: HomeIcon,
+  },
+  {
+    name: "Explore",
+    icon: ExploreIcon,
+  },
+  {
+    name: "Notification",
+    icon: NotificationIcon,
+  },
+  {
+    name: "Messages",
+    icon: MessagesIcon,
+  },
+  {
+    name: "Bookmark",
+    icon: BookmarkIcon,
+  },
+  {
+    name: "Lits",
+    icon: LitsIcon,
+  },
+  {
+    name: "Profile",
+    icon: ProfileIcon,
+  },
+  {
+    name: "More",
+    icon: MoreIcon,
+  },
+];
+
+
 
 
 const Sidebar = () => {
+
+  const [active, setActive] = useState('Home');
+
+  const handleClickMenuItem = (name) => {
+    setActive(name)
+  };
+
   return (
-    <aside className="w-72 bg-blue-200">
-      SideBar
-      <HomeIcon />
-      <ExploreIcon />
-      <NotificationIcon />
-      <MessagesIcon />
-      <BookmarkIcon />
-      <LitsIcon />
-      <ProfileIcon />
-      <MoreIcon />
-      <MediaIcon className="w-7 h-7" />
-      <GifIcon className="w-7 h-7" />
-      <SurveyIcon className="w-7 h-7" />
-      <EmojiyIcon className="w-7 h-7" />
-      <ScheduleIcon className="w-7 h-7" />
+    <aside className="flex flex-col justify-between w-72 px-2">
+      <div>
+        <div className="mt-1 mb-4 ml-1 p-2 flex items-center justify-center rounded-full w-12 h-12 hover:bg-gray-lightest transfom transition-colors duration-200">
+          <img src={twitterLogo} alt="" className="w-9 h-9" />
+        </div>
+        <nav className="mb-4">
+          <ul>
+            {navButtons.map(({name, icon}) => {
+              return (
+                <NavigationButton key={name} name={name} Icon={icon} active={active} onMenuItemClick={handleClickMenuItem} />
+              );
+            })}
+          </ul>
+        </nav>
+        <button className="bg-primary-base hover:bg-primary-dark text-white rounded-full py-3 px-8 w-11/12 shadow-lg transfom transition-colors duration-200">
+          Tweet
+        </button>
+      </div>
+      <UserBox />
     </aside>
-  )
-}
+  );
+};
 
-export default Sidebar
-
+export default Sidebar;
